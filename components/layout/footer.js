05e767d8ee1db983316b9162f3919bf4c5d119ee
Monokuro <07akhmadnur@gmail.com>
2023-05-29 00:54:36 +0700

    Change footer

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,8 +1,10 @@
+import dayjs from "dayjs";
+
 const Footer = () => {
   return (
     <footer className="bg-gray-900">
       <div className="container w-full mx-auto flex items-center justify-center px-2 py-4 text-sm md:py-5 text-gray-300 font-light">
-        @2023 Green Energy Project
+        Copyright &#169; {dayjs().format("YYYY")} Green Energy Project
       </div>
     </footer>
   );
